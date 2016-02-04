0c73015be623ec27806b0a8e78dc061dbf03922c
Kyle Stevenson <kyle@kylestevenson.me>
2016-02-03 19:43:07 -0800

    remove table-cell display property from container class

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,10 @@ var elixir = require('laravel-elixir');
 require('laravel-elixir-vueify');
 
 elixir(function(mix) {
-    mix.sass('app.scss');
-    mix.browserify('app.js');
+  mix.sass('app.scss')
+    .browserify('app.js')
+    .version([
+      'css/app.css',
+      'js/app.js'
+    ]);
 });
